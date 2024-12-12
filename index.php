60f140f3690a534ee4c68d38f71c7d897eeb1608
Thomas Günther <mail@medienbaecker.com>
2024-12-12 12:32:08 +0100

    Fix language option

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 @include_once __DIR__ . '/vendor/autoload.php';
 
 use Kirby\Cms\App as Kirby;
+use Kirby\Toolkit\Str;
 use Vanderlee\Syllable\Syllable;
 
 Syllable::setCacheDir(kirby()->root('cache'));
@@ -10,12 +11,11 @@ Syllable::setCacheDir(kirby()->root('cache'));
 Kirby::plugin('medienbaecker/hyphenate', [
   'options' => [
     'minWordLength' => 10,
-    'language' => function () {
-      return kirby()->language() ? kirby()->language()->code() : 'en';
-    }
+    'language' => Str::replace(kirby()->language()->locale()[0], '_', '-') ?? 'en-gb'
   ],
   'hooks' => [
     'kirbytext:after' => function (string|null $text) {
+
       $syllable = new Syllable(option('medienbaecker.hyphenate.language'));
       $syllable->setMinWordLength(option('medienbaecker.hyphenate.minWordLength'));
 
